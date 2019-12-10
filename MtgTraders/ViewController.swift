e93c3dbf530ab2180990bbfd9cded7ec93681b2a
Matthew Patterson <infestedd2@icloud.com>
2019-12-09 22:34:13 -0600

    Initial Commit

---
+++ MtgTraders/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MtgTraders
+//
+//  Created by Matthew Patterson on 12/9/19.
+//  Copyright © 2019 Matthew Patterson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
